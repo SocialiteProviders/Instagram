65a0e866afc19e5775da95181736507314cc9d91
Chris <chris@redbeed.com>
2020-04-17 11:56:04 +0200

    Add Longterm function

--- Provider.php
+++ Provider.php
@@ -23,16 +23,14 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected $scopes = ['user_profile'];
 
-    protected $feilds = 'username,account_type,media_count,media';
+    protected $fields = 'username,account_type,media_count,media';
 
     /**
      * {@inheritdoc}
      */
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase(
-            'https://api.instagram.com/oauth/authorize', $state
-        );
+        return $this->buildAuthUrlFromBase('https://api.instagram.com/oauth/authorize', $state);
     }
 
     /**
@@ -43,6 +41,16 @@ class Provider extends AbstractProvider implements ProviderInterface
         return 'https://api.instagram.com/oauth/access_token';
     }
 
+    protected function getLongTermTokenUrl()
+    {
+        return 'https://graph.instagram.com/access_token';
+    }
+
+    protected function getRefreshLongTermTokenUrl()
+    {
+        return 'https://graph.instagram.com/refresh_access_token';
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -50,16 +58,19 @@ class Provider extends AbstractProvider implements ProviderInterface
     {
         $query = [
             'access_token' => $token,
-            'fields' => $this->feilds
+            'fields' => $this->fields,
         ];
 
         $response = $this->getHttpClient()->get(
-            'https://graph.instagram.com/me', [
-            'query' => $query,
-            'headers' => [
-                'Accept' => 'application/json',
-            ],
-        ]);
+            'https://graph.instagram.com/me',
+            [
+                'query' => $query,
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]
+        );
+
         return json_decode($response->getBody()->getContents(), true);
     }
 
@@ -68,14 +79,16 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function mapUserToObject(array $user)
     {
-        return (new User())->setRaw($user)->map([
-            'id' => $user['id'],
-            'nickname' => $user['username'],
-            'name' => $user['username'],
-            'account_type' => $user['account_type'],
-            'media' => $user['media']['data'],
-            'avatar' => null,
-        ]);
+        return (new User())->setRaw($user)->map(
+            [
+                'id' => $user['id'],
+                'nickname' => $user['username'],
+                'name' => $user['username'],
+                'account_type' => $user['account_type'],
+                'media' => $user['media']['data'],
+                'avatar' => null,
+            ]
+        );
     }
 
     /**
@@ -83,13 +96,58 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     public function getAccessToken($code)
     {
-        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'form_params' => $this->getTokenFields($code),
-        ]);
+        $response = $this->getHttpClient()->post(
+            $this->getTokenUrl(),
+            [
+                'form_params' => $this->getTokenFields($code),
+            ]
+        );
 
         $this->credentialsResponseBody = json_decode($response->getBody(), true);
 
         return $this->parseAccessToken($response->getBody());
+    }
+
+    public function refreshLongTermAccessToken($token) {
+        $query = [
+            'grant_type' => 'ig_refresh_token',
+            'access_token' => $token,
+        ];
+
+        $response = $this->getHttpClient()->get(
+            $this->getRefreshLongTermTokenUrl(),
+            [
+                'query' => $query,
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function getLongTermAccessToken($oldToken)
+    {
+        $query = array_merge(
+            parent::getTokenFields(null),
+            [
+                'grant_type' => 'ig_exchange_token',
+                'access_token' => $oldToken,
+            ]
+        );
+
+        $response = $this->getHttpClient()->get(
+            $this->getLongTermTokenUrl(),
+            [
+                'query' => $query,
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     /**
@@ -97,9 +155,12 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getTokenFields($code)
     {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
+        return array_merge(
+            parent::getTokenFields($code),
+            [
+                'grant_type' => 'authorization_code',
+            ]
+        );
     }
 
 }
